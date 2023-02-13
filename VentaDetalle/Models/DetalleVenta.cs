8af27f61c9540162050ce244654666426da3135e
kikemadrigal <carlosmad03@gmail.com>
2023-02-13 00:09:28 -0500

    Agregar archivos de proyecto.

---
+++ VentaDetalle/Models/DetalleVenta.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace VentaDetalle.Models
+{
+    public class DetalleVenta
+    {
+        public int IdVentaDetalle { get; set; }
+
+        public string Producto { get; set; }
+
+        public decimal Precio { get; set;}
+
+        public int Cantidad { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
---
+++ VentaDetalle/Models/Venta.cs
@@ -0,0 +1,15 @@
+﻿namespace VentaDetalle.Models
+{
+    public class Venta
+    {
+        public int IdVenta { get; set; }
+
+        public string NumeroDocumento { get; set; }
+
+        public string RazonSocial { get; set; }
+
+        public decimal Total { get; set; }
+
+        public List<DetalleVenta> ListVentaDetalle { get; set; }
+    }
+}
